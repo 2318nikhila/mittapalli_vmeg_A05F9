d15204b127ee769129b85e321c74dbf26972be25
2318nikhila <31914842+2318nikhila@users.noreply.github.com>
2017-09-13 14:54:04 +0530

    Add files via upload

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -64,7 +64,7 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void setStudent(Student student, int index) {
 		// Add your implementation her
-if(index > this.getStudents().length || index < 0  ) 
+if(index > this.getStudents().length || index < 0||student == null  ) 
 throw new IllegalArgumentException();
 		this.getStudents()[index] = student; 
 	
